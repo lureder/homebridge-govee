173d2cc2511820d3830867d82d8beb338e55b169
Derek Lurette <derek_lurette@icloud.com>
2025-02-25 15:27:34 -0500

    Delete homebridge-govee-10.16.0 directory

--- homebridge-govee-10.16.0/lib/index.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'node:module'
-
-import goveePlatform from './platform.js'
-
-const require = createRequire(import.meta.url)
-const plugin = require('../package.json')
-
-export default hb => hb.registerPlatform(plugin.alias, goveePlatform)
